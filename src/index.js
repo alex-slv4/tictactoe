e9290f426cfc4da37ac091da0295173602e3bbae
Alexander Slavschik <kvinty@icloud.com>
2018-12-17 15:26:20 +0300

    Basic pixi drawing

--- src/index.js
+++ src/index.js
@@ -1,2 +1,77 @@
-var app = new new PIXI.Application();
-document.body.appendChild(app.view);
+var globals = {app: null, game: null};
+
+globals.app = new PIXI.Application();
+document.body.appendChild(globals.app.view);
+
+// window resize logic
+(function(app){
+    function resize(){
+        app.renderer.resize(app.view.clientWidth, app.view.clientHeight);
+    }
+    window.addEventListener("resize", resize.bind(this));
+    resize();
+})(globals.app);
+
+// model
+function GameModel(rows, column) {
+
+    function CellModel(owner) {
+        this.owner = owner;
+        this.toString = function() {
+            return "[CellModel" + this.owner + "]";
+        }
+    }
+    CellModel.OWNER_NONE = "";
+    CellModel.OWNER_X = "X";
+    CellModel.OWNER_O = "O";
+
+    this.field = [];
+    for (var i = 0; i < rows; i++) {
+        this.field[i] = [];
+        for (var j = 0; j < column; j++) {
+            this.field[i][j] = new CellModel(CellModel.OWNER_NONE);
+        }
+    }
+}
+
+// view
+function GameView(gameFacade) {
+
+    function CellView() {
+        PIXI.Container.call(this);
+
+        var circle = new PIXI.Graphics().beginFill(0xff0000).drawCircle(0, 0, 20).endFill();
+        this.addChild(circle);
+
+        this.on("pointerdown", this.onDown.bind(this)) // TODO: call the off after destroy
+
+    }
+    CellView.CELL_WIDTH = 100;
+    CellView.CELL_HEIGHT = 100;
+    CellView.prototype = new PIXI.Container();
+
+    this.gameFacade = gameFacade;
+    this.draw = function() {
+        var field = this.gameFacade.model.field;
+        for (var i = 0; i < field.length; i++) {
+            for (var j = 0; j < field[i].length; j++) {
+                var cell = new CellView(field[i][j]);
+                cell.position.set(i * CellView.CELL_WIDTH, j * CellView.CELL_HEIGHT);
+                this.addChild(cell);
+            }
+        }
+    }
+}
+GameView.prototype = new PIXI.Container();
+
+// the Game Facade
+function Game(rows, column) {
+    this.model = new GameModel(rows, column);
+    this.view = new GameView(this);
+}
+globals.app.start();
+
+globals.game = new Game(5, 5);
+globals.app.stage.addChild(globals.game.view);
+globals.game.view.draw();
+console.log(globals.game);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var DisableOutputWebpackPlugin = require('disable-output-webpack-plugin');
+var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
 var path = require('path');
 
 module.exports = {
@@ -10,9 +13,16 @@ module.exports = {
         filename: 'index_bundle.js'
     },
     plugins: [
+        new DisableOutputWebpackPlugin(),
+
         new HtmlWebpackPlugin({
+            filename: "index.html",
             template: path.resolve(__dirname, "src/template.html")
         }),
-        new CopyWebpackPlugin([{from: path.join(__dirname, 'node_modules', 'pixi.js', 'dist'), to: "lib"}], {})
+        new HtmlWebpackInlineSourcePlugin(),
+        new CopyWebpackPlugin([
+            {from: path.join(__dirname, 'node_modules', 'pixi.js', 'dist'), to: "lib"},
+            {from: path.join(__dirname, 'src', 'index.js')}
+            ], {})
     ]
 };
